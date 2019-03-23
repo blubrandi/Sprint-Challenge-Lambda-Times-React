e5a92bfe8cb9c410b6fb32d74c9611532eee84f0
blubrandi <blubrandi0@gmail.com>
2019-03-23 12:45:16 -0600

    Moved styles from index.css to styled-components in Header.js

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,17 +1,60 @@
 import React from 'react';
 
-import '../CSS/index.css'
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const SiteHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+@media (min-width: 1280px) {
+    width: 1280px;
+}
+
+h1 {
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+}
+span {
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+}
+
+date {
+  margin-left: 25px;
+  flex: 1;
+}
+
+temp {
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+}
+`
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">MARCH 23, 2018</span>
+    <SiteHeader>
+      <date>MARCH 23, 2018</date>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <temp>98°</temp>
+    </SiteHeader>
   )
 }
 
