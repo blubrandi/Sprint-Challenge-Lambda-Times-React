7cdf8f755e9113502af774d1c3ea1e6df1e52746
blubrandi <blubrandi0@gmail.com>
2019-03-23 12:20:14 -0600

    Added card filtering

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,6 +3,8 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
+import './CSS/index.css'
+
 const App = () => {
   return (
     <div className="App">
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import '../CSS/index.css'
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <span className="date">MARCH 23, 2018</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
